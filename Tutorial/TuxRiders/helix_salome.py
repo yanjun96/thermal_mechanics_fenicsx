2189594c1d1c51550371f292dcab020eebf13d86
Yanjun96 <simpson1996@live.cn>
2024-02-12 19:06:31 +0100

    tutorial

---
+++ Tutorial/TuxRiders/helix_salome.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+###
+### This file is generated automatically by SALOME v9.12.0 with dump python functionality
+###
+
+import sys
+import salome
+
+salome.salome_init()
+import salome_notebook
+notebook = salome_notebook.NoteBook()
+sys.path.insert(0, r'/home/yanjun/Documents/FEniCSx/Tutorial/TuxRiders')
+
+
+if salome.sg.hasDesktop():
+  salome.sg.updateObjBrowser()
