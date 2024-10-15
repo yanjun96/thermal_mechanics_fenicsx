18d8c28b626b34fe0237e3c39696b071c39f107c
Yanjun96 <simpson1996@live.cn>
2024-10-15 20:33:09 +0200

    mesh_brake_pad

--- mech_disc/disc_f.py
+++ mech_disc/disc_f.py
@@ -279,8 +279,8 @@ def mesh_brake_disc(min_mesh, max_mesh, filename, mesh_type):
     return c
     return notice
 
-#########################33333333333333333###########################################################################  6
-def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   ##unfinished
+#########################33333333333333333#####################################################
+def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   
     import gmsh
     import sys
     import math
@@ -306,11 +306,6 @@ def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   ##unfinished
     # x (double), y (double), z (double), dx (double), dy (double), dz (double),
     # r (double), tag = -1 (integer), angle = 2*pi (double)
     
-    # brake disc
-    outer_disc  = gmsh.model.occ.addCylinder(0,0,0,  0, 0, z1,  rd_o)
-    inner_disc  = gmsh.model.occ.addCylinder(0,0,0,  0, 0, z1,  rd_i)
-    disk = gmsh.model.occ.cut([(3, outer_disc)], [(3, inner_disc)])
-    
     # rubbing elements
     x_co, y_co = get_rub_coordinate()
     
@@ -320,40 +315,34 @@ def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   ##unfinished
        tag = gmsh.model.occ.addCylinder(x, y, z1, 0, 0, z2, r_rub)
        globals()[var_name] = tag
        rub_list.append(globals()[var_name])
+    gmsh.model.occ.synchronize()
     
     # brake pad, in [(3, )],3 means dimension, cut the common place, out - inner
     outer_pad  = gmsh.model.occ.addCylinder(0,0,z1+z2,  0, 0, z3,  rp_o, 50, angle)
     inner_pad  = gmsh.model.occ.addCylinder(0,0,z1+z2,  0, 0, z3,  rp_i, 51, angle)
     pad = gmsh.model.occ.cut([(3, outer_pad)], [(3, inner_pad)]) 
+    gmsh.model.occ.synchronize()
     
     # Initialize the shell with the first rub
     rub_list = [rub1, rub2, rub3, rub4, rub5, rub6, rub7, 
                 rub8, rub9, rub10, rub11, rub12, rub13, rub14, rub15, rub16, rub17, rub18]
-    shell = gmsh.model.occ.fuse([(3, outer_pad)], [(3, rub_list[0])], 70)
-    for i in range(len(rub_list) - 1):
+    shell = gmsh.model.occ.fuse([(3, outer_pad)], [(3, rub_list[0])], 70) # 70 is just a name
+    
+    for i in range(len(rub_list) - 1): # shell is the combination volume
         shell = gmsh.model.occ.fuse([(3, 70 + i)], [(3, rub_list[i + 1])], 71 + i)
     gmsh.model.occ.synchronize()
     
-    # Add physical group, this step should after synchronize to make sure success
-    # https://gitlab.onelab.info/gmsh/gmsh/blob/master/tutorials/python/t1.py#L115
-    
     # Volumes: 31,32 brake disc and pad.
     volumes = gmsh.model.occ.getEntities(dim = 3)
-    gmsh.model.addPhysicalGroup(3, volumes[0],  31)
-    gmsh.model.addPhysicalGroup(3, volumes[1],  32)
+    gmsh.model.addPhysicalGroup(3, volumes[0],  32)
     
     # Surfaces: brake disc, 21 = friction surface
     surfaces = gmsh.model.occ.getEntities(dim = 2)
-    gmsh.model.addPhysicalGroup(2, (2,6), 21)
     
     # Rubbing elements, from 1 to 19, here 32 is the origin name tag of rub surface(32-49)
-    rublist = list(range(32,50))
+    rublist = list(range(25,43))
     for rub in rublist:
-        gmsh.model.addPhysicalGroup(2, (2, rub), rub-31)
-
-    if mesh_type == 'hexa':
-       gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 2)
-    
+        gmsh.model.addPhysicalGroup(2, (0, rub), rub-24) #(0,rub), 0 is empty tag.  
     # for the rubbing elements, P13 of UIC 541-3
     # Sinter material, 200 cm2, 18 rubbing elemets, r = 1.88 cm
     # Mesh size
@@ -369,8 +358,8 @@ def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   ##unfinished
     
     return c
     return notice
-
-#########################33333333333333333###########################################################################
+    
+#########################33333333333333333#####################################################
 def target_facets(domain,x_co,y_co,S_rub_circle):
     from dolfinx.mesh import locate_entities
     from dolfinx import mesh
