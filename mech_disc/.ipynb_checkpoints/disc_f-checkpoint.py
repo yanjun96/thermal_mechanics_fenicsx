139ef12aa9855ff174b378a3779fceb2582a7a4e
Yanjun96 <simpson1996@live.cn>
2024-10-23 21:02:19 +0200

    try penalty method

--- mech_disc/.ipynb_checkpoints/disc_f-checkpoint.py
+++ mech_disc/.ipynb_checkpoints/disc_f-checkpoint.py
@@ -257,9 +257,7 @@ def mesh_brake_disc(min_mesh, max_mesh, filename, mesh_type,pad_v_tag):
     
     
     gmsh.initialize()
-    
-
-    
+        
     # all the unit is mm
     # z1, z2, z3 is the height of brake disc, rubbing elemetn, pad lining in Z direction
     # rd_outer, rd_inner is for brake disc, rp_outer, rp_inner is for brake pad radiu. r_rub is for rubbing elements
@@ -343,87 +341,6 @@ def mesh_brake_disc(min_mesh, max_mesh, filename, mesh_type,pad_v_tag):
     return c
     return notice
 
-#########################33333333333333333#####################################################
-def mesh_brake_pad(min_mesh, max_mesh, filename, mesh_type):   
-    import gmsh
-    import sys
-    import math
-    import os
-    import numpy as np
-    
-    gmsh.initialize()
-    
-    
-    # all the unit is mm
-    # z1, z2, z3 is the height of brake disc, rubbing elemetn, pad lining in Z direction
-    # rd_outer, rd_inner is for brake disc, rp_outer, rp_inner is for brake pad radiu. r_rub is for rubbing elements
-    # angle1 is brake pad in degree system, angle is in radians system
-    
-    z1, z2, z3 = 20, 33, 30
-    rd_o, rd_i = 320, 175 
-    r_rub = 18.8
-    rp_o, rp_i = 303, 178
-    
-    angle1 = 80
-    angle = angle1 / 360 * 2 * math.pi
-    
-    # gmsh.model.occ.addCylinder
-    # x (double), y (double), z (double), dx (double), dy (double), dz (double),
-    # r (double), tag = -1 (integer), angle = 2*pi (double)
-    
-    # rubbing elements
-    x_co, y_co = get_rub_coordinate()
-    
-    rub_list = []
-    for i, (x, y) in enumerate(zip(x_co, y_co), start=1):
-       var_name = f"rub{i}"
-       tag = gmsh.model.occ.addCylinder(x, y, z1, 0, 0, z2, r_rub)
-       globals()[var_name] = tag
-       rub_list.append(globals()[var_name])
-    gmsh.model.occ.synchronize()
-    
-    # brake pad, in [(3, )],3 means dimension, cut the common place, out - inner
-    outer_pad  = gmsh.model.occ.addCylinder(0,0,z1+z2,  0, 0, z3,  rp_o, 50, angle)
-    inner_pad  = gmsh.model.occ.addCylinder(0,0,z1+z2,  0, 0, z3,  rp_i, 51, angle)
-    pad = gmsh.model.occ.cut([(3, outer_pad)], [(3, inner_pad)]) 
-    gmsh.model.occ.synchronize()
-    
-    # Initialize the shell with the first rub
-    rub_list = [rub1, rub2, rub3, rub4, rub5, rub6, rub7, 
-                rub8, rub9, rub10, rub11, rub12, rub13, rub14, rub15, rub16, rub17, rub18]
-    shell = gmsh.model.occ.fuse([(3, outer_pad)], [(3, rub_list[0])], 70) # 70 is just a name
-    
-    for i in range(len(rub_list) - 1): # shell is the combination volume
-        shell = gmsh.model.occ.fuse([(3, 70 + i)], [(3, rub_list[i + 1])], 71 + i)
-    gmsh.model.occ.synchronize()
-    
-    # Volumes: 31,32 brake disc and pad.
-    volumes = gmsh.model.occ.getEntities(dim = 3)
-    gmsh.model.addPhysicalGroup(3, volumes[0],  32)
-    
-    # Surfaces: brake disc, 21 = friction surface
-    surfaces = gmsh.model.occ.getEntities(dim = 2)
-    
-    # Rubbing elements, from 1 to 19, here 32 is the origin name tag of rub surface(32-49)
-    rublist = list(range(25,43))
-    for rub in rublist:
-        gmsh.model.addPhysicalGroup(2, (0, rub), rub-24) #(0,rub), 0 is empty tag.  
-    # for the rubbing elements, P13 of UIC 541-3
-    # Sinter material, 200 cm2, 18 rubbing elemets, r = 1.88 cm
-    # Mesh size
-    gmsh.option.setNumber("Mesh.MeshSizeMin", min_mesh)
-    gmsh.option.setNumber("Mesh.MeshSizeMax", max_mesh)
-    
-    # Mesh file save
-    gmsh.model.mesh.generate(3)
-    c = gmsh.write(filename + ".msh")
-    notice = print("NOTICE:" + filename + " has been meshed successfully and saved as " + filename + ".msh")   
-  
-    gmsh.finalize()
-    
-    return c
-    return notice
-    
 #########################33333333333333333#####################################################
 def target_facets(domain,x_co,y_co,S_rub_circle):
     from dolfinx.mesh import locate_entities
@@ -1078,7 +995,7 @@ def plot_T_pad(domain_pad, T_pad):
     return plotter
 
 #######################################################
-def plot_S_pad(Vu,u_d ):
+def plot_S_pad(Vu,u_d,scale_factor ):
     u_topology, u_cell_types, u_geometry = plot.vtk_mesh(Vu)        # get mesh data
     u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry) # plot grid
     u_3D = np.zeros((u_geometry.shape[0], 3))
@@ -1086,7 +1003,7 @@ def plot_S_pad(Vu,u_d ):
     u_grid.point_data["Displacement/ mm"] = u_3D
 
     u_grid.set_active_vectors("Displacement/ mm")
-    warped = u_grid.warp_by_vector("Displacement/ mm", factor=1)
+    warped = u_grid.warp_by_vector("Displacement/ mm", factor=scale_factor )
     plotter = pyvista.Plotter()
     plotter.window_size = (800, 400)
     sargs = dict(title_font_size=25, label_font_size=20,  color="black",
